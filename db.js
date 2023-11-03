a70185caf4d7d12d66e39aa7ef82eceef541a753
Victoria <sweet.victoria1218@gmail.com>
2023-11-02 21:15:42 -0500

    Get mysql connection working

--- db.js
+++ db.js
@@ -1,44 +1,21 @@
-// const mariadb = require("mariadb");
-
-// const pool = mariadb.createPool({
-//   host: "penguin",
-//   port: 3306,
-//   user: "root",
-//   password: "your-password",
-//   database: "employee_tracker",
-// });
-
-// if (pool) {
-//   console.log("Connected to the database");
-// }
-
-// // Expose a method to establish connection with MariaDB SkySQL
-// module.exports =
-//   // Object.freeze(
-//   {
-//     pool: pool,
-//   };
-// // );
-
 const mysql = require("mysql2");
 
+const port = 3306;
 const db = mysql.createConnection({
-  host: "penguin",
-  port: 3306,
+  host: "127.0.0.1",
+  port: port,
   user: "root",
   password: "your-password",
   database: "employee_tracker",
-  acquireTimeout: 1000000,
-  connectTimeout: 1000000,
-  waitForConnections: true,
-  queueLimit: 0,
 });
-// and to make sure if you are connected or not use this:
 
 db.connect((error) => {
   if (error) {
     console.log(error);
   } else {
-    console.log("Connected..");
+    console.log(`Connected to database on port ${port}`);
   }
 });
+
+// export the db connection for use in app
+module.exports = db;
